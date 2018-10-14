89ef9776872b4deec7c048972fd54526fd02cc68
Fabien <rajaonarisonfabien@yahoo.fr>
2018-10-14 20:00:29 +0200

    procfile edit

--- server/app.js
+++ server/app.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const path = require("path");
+const bodyParser = require('body-parser');
 const app = express();
 const server = require('http').createServer(app);
 var io = require('socket.io')(server);
@@ -6,10 +8,12 @@ const PORT = process.env.PORT || 8080;
 
 app.use(express.static(__dirname + '/dist/drawus'));
 
-console.log(__dirname + '/../src/index.html');
+// console.log(__dirname + '/../dist/drawus/index.html');
 
-app.get('/*', function (req, res) {
-    res.sendFile(path.join(__dirname + '/../dist/index.html'));
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname + '/../dist/drawus/index.html'));
 });
 
 io.on('connection', (socket) => {
