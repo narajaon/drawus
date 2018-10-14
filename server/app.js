7f54bff82f7b90dcedc545043822b8eaea9d45a9
Fabien <rajaonarisonfabien@yahoo.fr>
2018-10-14 19:34:55 +0200

    procfile edit

--- server/app.js
+++ server/app.js
@@ -1,7 +1,16 @@
-const app = require('express')();
+const express = require('express');
+const app = express();
 const server = require('http').createServer(app);
 var io = require('socket.io')(server);
 const PORT = process.env.PORT || 8080;
+
+app.use(express.static(__dirname + '/dist/drawus'));
+
+console.log(__dirname + '/../src/index.html');
+
+app.get('/*', function (req, res) {
+    res.sendFile(path.join(__dirname + '/../src/index.html'));
+});
 
 io.on('connection', (socket) => {
     console.log('Client connected');
